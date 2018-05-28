2eca53a3c39c3a204a56afdb85c182412015649c
Chetan Saraf <onlinechetan@gmail.com>
2018-05-28 17:53:35 -0500

    Refactor: equals and == operator

---
+++ src/main/java/com/mylearning/EqualsAndEqualEqual.java
@@ -0,0 +1,15 @@
+package com.mylearning;
+
+/**
+ * Difference between equals to and ==.
+ */
+public class EqualsAndEqualEqual {
+    static String str1 = "one";
+    static String first = "one";
+
+    public static void main(String[] args) {
+        System.out.println("using == operator " + (str1 == first));  //== is operator. Will return false
+        System.out.println("using equals method " + str1.equals(first)); //equals is a method that could be overridden 
+
+    }
+}
